adc7aff9b3d7a8e69044aa79e6a33cce67ef106a
sean regan <rego363@gmail.com>
2018-09-04 12:19:48 +0100

    First commit

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour {
+
+	public void doQuit()
+    {
+        Debug.Log("Has quit");
+        Application.Quit();
+    }
+}
